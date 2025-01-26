84c8e1ac1cbcb570694b1bf4d9158103d4935bdc
SimonHegele <103111576+SimonHegele@users.noreply.github.com>
2025-01-26 09:07:33 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+name='rnaQUASTcompare',
+version='0.1',
+description='Comparing multiple transcriptome assemblies assesed by rnaQUAST',
+author='SimonHegele',
+install_requires=[
+'matplotlib'
+'numpy',
+'pandas',
+],
+)
